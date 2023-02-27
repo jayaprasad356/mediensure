df309d186531d1513ca223ff4714bf8f2c54feaf
chhagan <chhaganswami1@gmail.com>
2023-02-27 16:51:10 +0530

    chagan

---
+++ app/src/main/java/com/graymatter/mediensure/RadiologyNetwork.java
@@ -0,0 +1,14 @@
+package com.graymatter.mediensure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RadiologyNetwork extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_radiology_network);
+    }
+}
